2ffbca42ce9436c62dc811e91e1c6e29e3321b7f
SHADOWEWOK <shadowewok@gmail.com>
2025-10-11 03:16:44 -0400

    Player Ver 1
    Added:
    -PlayerCharacter Folder
    -Kevin Iglesias Model & Animations, for Character 0, test character.
    -Character 0's movement and animations
    -Scene CharTest, for testing player models

---
+++ The Riftkeepers/Assets/PlayerCharacters/Character 0/CharMovement.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CharMovement : MonoBehaviour
+{
+    public InputActionAsset InputActions;
+
+    private InputAction pMoveAct;
+    private InputAction pLookAct;
+
+    private Vector2 pVelocity;
+    private Vector2 pGaze;
+
+    private Animator pAnimator;
+    private Rigidbody pRigBod;
+
+    public float pSpeed = 5;
+    public float pRotSpd = 5;
+    public float pJmpSpd = 5;
+
+
+    private void OnEnable(){
+
+        InputActions.FindActionMap("Player").Enable();
+
+
+    }
+
+    private void OnDisable()
+    {
+
+        InputActions.FindActionMap("Player").Disable();
+
+
+    }
+
+    private void Awake(){
+
+        pMoveAct = InputSystem.actions.FindAction("Move");
+        pLookAct = InputSystem.actions.FindAction("Look");
+
+        pAnimator = GetComponent<Animator>();
+        pRigBod = GetComponent<Rigidbody>();
+
+    }
+
+    private void Update(){
+        
+        pVelocity = pMoveAct.ReadValue<Vector2>();
+        pGaze = pLookAct.ReadValue<Vector2>();
+
+
+
+
+    }
+
+    private void FixedUpdate(){
+        Walking();
+        Rotating();
+    }
+
+
+    private void Walking(){
+        pAnimator.SetFloat("speedY", pVelocity.y);
+        pAnimator.SetFloat("speedX", pVelocity.x);
+        pRigBod.MovePosition(pRigBod.position + transform.forward * pVelocity.y * pSpeed * Time.deltaTime);
+        
+      
+    } 
+
+    private void Rotating(){
+
+    
+
+            float pRot = pVelocity.x * pRotSpd * Time.deltaTime;
+            Quaternion deltaRot = Quaternion.Euler(0, pRot, 0);
+            pRigBod.MoveRotation(pRigBod.rotation * deltaRot);
+        
+        
+
+    }
+}
