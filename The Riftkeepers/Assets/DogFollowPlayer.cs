0da104c198385506557e87c2ced5c0c917b279d4
Faheem Lakhani <faheemlakhani786@gmail.com>
2025-11-08 10:01:18 -0500

    Added NavMesh system and NPC dog

---
+++ The Riftkeepers/Assets/DogFollowPlayer.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DogFollowPlayer : MonoBehaviour
+{
+    public Transform player;
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        if (player == null)
+            player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        if (player != null)
+        {
+            agent.SetDestination(player.position);
+        }
+    }
+}
